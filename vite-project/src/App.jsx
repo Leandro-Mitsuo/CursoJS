a6819279dc7bd3f03f22ae8581847a0e9dabebcf
Leandro-Mitsuo <leandromitsuo@gmail.com>
2024-11-02 11:02:36 -0300

    OK

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Next-Health</h1>
+      <h1>Teste APP</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
